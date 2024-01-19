285021527eddebb61c6fa343759c7042f6c98ede
Rupeshrauniyar <151705269+Rupeshrauniyar@users.noreply.github.com>
2024-01-19 23:18:59 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+var loader = document.querySelector('.loader')
+var h3 = document.querySelector('#h3')
+
+
+setTimeout(function(){
+loader.style.top="-100%"
+},2200)
+setTimeout(function(){
+h3.style.opacity="0"
+},1000)
